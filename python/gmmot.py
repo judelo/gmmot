5329f2c00d940cba3d15e794c1264955d68e5690
judelo <julie.delon@parisdescartes.fr>
2019-08-26 10:14:02 +0200

    added color transfer and modified all files

--- python/gmmot.py
+++ python/gmmot.py
@@ -28,16 +28,17 @@ def GaussianW2(m0,m1,Sigma0,Sigma1):
 
 def GaussianMap(m0,m1,Sigma0,Sigma1,x):
     # Compute the OT map between two Gaussians
-    # m0 and m1 must be 2D arrays of size dx1
+    # m0 and m1 must be 2D arrays of size 1xd
     # Sigma0 and Sigma1 must be 2D arrays of size dxd
-    # x can be a matrix of size d x n,
+    # x can be a matrix of size n x d,
     # each column of x is a vector to which the function is applied
     d = m0.shape[0]
-    m0 = m0.reshape(d,1)
+    m0 = m0.reshape(1,d)
+    m1 = m1.reshape(1,d)
     Sigma0 = Sigma0.reshape(d,d)
     Sigma1 = Sigma1.reshape(d,d)
     Sigma  = np.linalg.inv(Sigma0)@spl.sqrtm(Sigma0@Sigma1)
-    Tx        = m1+Sigma@(x-m0)
+    Tx        = m1+(x-m0)@Sigma
     return Tx
 
 
@@ -75,11 +76,11 @@ def GW2_map(pi0,pi1,mu0,mu1,S0,S1,wstar,x):
     weightmean = np.zeros(n)
     Tmean      = np.zeros((n,n))     # averaged map
     Tmap       = np.zeros((n,n))     # display of the multivalued map
-
+    
     for k in range(K0):
         for l in range(K1):
             if wstar[k,l]!=0:
-                T[k,l,:] = GaussianMap(mu0[k],mu1[l],S0[k],S1[l],x)
+                T[k,l,:] = GaussianMap(mu0[k],mu1[l],S0[k],S1[l],x).reshape(n,)
                 for i in range(n):
                     Ti             = int(max(min(T[k,l,i],1),0)*99)
                     Tmap[i,Ti]    += wstar[k,l]*sps.norm.pdf(x[i],loc=mu0[k],scale=S0[k])
@@ -89,6 +90,41 @@ def GW2_map(pi0,pi1,mu0,mu1,S0,S1,wstar,x):
     tmpmean = np.uint(np.maximum(np.minimum(tmpmean,1),0)*99)
     for i in range(n):
         Tmean[i,tmpmean[i]] = weightmean[i]
-
+    
     return Tmap,Tmean
+
+
+def average_filter(u,r):
+    # uniform filter with a square (2*r+1)x(2*r+1) window
+    # u is a 2d image
+    # r is the radius for the filter
+    
+    (nrow, ncol)                                      = u.shape
+    big_uint                                          = np.zeros((nrow+2*r+1,ncol+2*r+1))
+    big_uint[r+1:nrow+r+1,r+1:ncol+r+1]               = u
+    big_uint                                          = np.cumsum(np.cumsum(big_uint,0),1)       # integral image
+    
+    out = big_uint[2*r+1:nrow+2*r+1,2*r+1:ncol+2*r+1] + big_uint[0:nrow,0:ncol] - big_uint[0:nrow,2*r+1:ncol+2*r+1] - big_uint[2*r+1:nrow+2*r+1,0:ncol]
+    out = out/(2*r+1)**2
+    
+    return out
+
+def guided_filter(u,guide,r,eps):
+    C           = average_filter(np.ones(u.shape), r)   # to avoid image edges pb
+    mean_u      = average_filter(u, r)/C
+    mean_guide  = average_filter(guide, r)/C
+    corr_guide  = average_filter(guide*guide, r)/C
+    corr_uguide = average_filter(u*guide, r)/C
+    var_guide   = corr_guide - mean_guide * mean_guide
+    cov_uguide  = corr_uguide - mean_u * mean_guide
+    
+    alph = cov_uguide / (var_guide + eps)
+    beta = mean_u - alph * mean_guide
+    
+    mean_alph = average_filter(alph, r)/C
+    mean_beta = average_filter(beta, r)/C
+    
+    q = mean_alph * guide + mean_beta
+    return q
+
 
